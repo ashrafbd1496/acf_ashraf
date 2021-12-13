7ce215eb0ac32b99f989de7f7010764c8c0fa332
ashrafbd1496 <asraf1496@gmail.com>
2021-12-13 19:16:39 +0600

    counter section done

--- front-page.php
+++ front-page.php
@@ -73,96 +73,55 @@ get_header();?>
       <section class="ftco-section bg-light ftco-no-pt">
          <div class="container">
             <div class="row">
-               <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                  <div class="d-block">
-                     <div class="icon d-flex mr-2">
-                        <span class="flaticon-accounting-1"></span>
+               <?php 
+                  $services = get_field('services','option');
+                  if($services){
+                     foreach($services as $service){
+                        ?> 
+                        <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
+                        <div class="d-block">
+                           <div class="icon d-flex mr-2">
+                              <i class="fa <?php echo $service['service_icon'];?>"></i>
+                           </div>
+                           <div class="media-body">
+                              <h3 class="heading"><?php echo $service['service_title'];?></h3>
+                              <p><?php echo $service['service_item_description'];?></p>
+                           </div>
+                        </div>
                      </div>
-                     <div class="media-body">
-                        <h3 class="heading">Accounting</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                  <div class="d-block">
-                     <div class="icon d-flex mr-2">
-                        <span class="flaticon-tax"></span>
-                     </div>
-                     <div class="media-body">
-                        <h3 class="heading">Tax, Compliance &amp; Payroll</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                  <div class="d-block">
-                     <div class="icon d-flex mr-2">
-                        <span class="flaticon-loan"></span>
-                     </div>
-                     <div class="media-body">
-                        <h3 class="heading">Financial Services</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
-                  <div class="d-block">
-                     <div class="icon d-flex mr-2">
-                        <span class="flaticon-budget"></span>
-                     </div>
-                     <div class="media-body">
-                        <h3 class="heading">Growth &amp; Funding Access</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                     </div>
-                  </div>
-               </div>
+                     <?php
+   
+                     }             
+                  }
+                  ?>
+                  
             </div>
          </div>
       </section>
       <section class="ftco-counter bg-light ftco-no-pt" id="section-counter">
          <div class="container">
             <div class="row">
-               <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-                  <div class="block-18 text-center">
-                     <div class="text">
-                        <strong class="number" data-number="50">0</strong>
+               <?php 
+                  $counters = get_field('counters','options');
+                  if($counters){
+                     foreach($counters as $counter){
+                        ?>
+                        <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
+                        <div class="block-18 text-center">
+                           <div class="text">
+                              <strong class="number" data-number="<?php echo $counter['counter_number'];?>">0</strong>
+                           </div>
+                           <div class="text">
+                              <span><?php echo $counter['counter_title'];?></span>
+                           </div>
+                        </div>
                      </div>
-                     <div class="text">
-                        <span>Years of Experienced</span>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-                  <div class="block-18 text-center">
-                     <div class="text">
-                        <strong class="number" data-number="8500">0</strong>
-                     </div>
-                     <div class="text">
-                        <span>Cases Completed</span>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-                  <div class="block-18 text-center">
-                     <div class="text">
-                        <strong class="number" data-number="20">0</strong>
-                     </div>
-                     <div class="text">
-                        <span>Awards Won</span>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-md-6 col-lg-3 d-flex justify-content-center counter-wrap ftco-animate">
-                  <div class="block-18 text-center">
-                     <div class="text">
-                        <strong class="number" data-number="50">0</strong>
-                     </div>
-                     <div class="text">
-                        <span>Expert Consultant</span>
-                     </div>
-                  </div>
-               </div>
+                     <?php
+                     }
+                  }
+               ?>
+              
+            
             </div>
          </div>
       </section>
