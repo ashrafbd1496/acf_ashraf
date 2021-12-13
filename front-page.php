018026e7ae0b76a33091a7c1347984b3d3aa05c0
ashrafbd1496 <asraf1496@gmail.com>
2021-12-13 20:57:16 +0600

    Testimonial Slider Section done

--- front-page.php
+++ front-page.php
@@ -130,88 +130,41 @@ get_header();?>
          <div class="container">
             <div class="row justify-content-center pb-5 mb-3">
                <div class="col-md-7 heading-section heading-section-white text-center ftco-animate">
-                  <span class="subheading">Testimonies</span>
-                  <h2>Happy Clients &amp; Feedbacks</h2>
+                  <?php 
+                     $testimonies = get_field('testimonials','options');             
+                  
+                  ?>
+                  <span class="subheading"><?php echo  $testimonies['testimonial_subtitle']; ?></span>
+                  <h2><?php echo  $testimonies['testimonial_title']; ?></h2>
                </div>
             </div>
             <div class="row ftco-animate">
                <div class="col-md-12">
                   <div class="carousel-testimony owl-carousel ftco-owl">
-                     <div class="item">
+                    <?php 
+                    $testimonies_slider = get_field('testimonies_slider','options');
+                    foreach($testimonies_slider as $testimoni_slide){
+                       ?>
+                        <div class="item">
                         <div class="testimony-wrap py-4">
                            <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
                            <div class="text">
-                              <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                              <p class="mb-4"><?php echo $testimoni_slide['tslider_description'];?></p>
                               <div class="d-flex align-items-center">
-                                 <div class="user-img" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/person_1.jpg)"></div>
+                                 <div class="user-img" style="background-image: url(<?php echo $testimoni_slide['tslider_image']['url'];?>)"></div>
                                  <div class="pl-3">
-                                    <p class="name">Roger Scott</p>
-                                    <span class="position">Marketing Manager</span>
+                                    <p class="name"><?php echo $testimoni_slide['tslider_name'];?></p>
+                                    <span class="position"><?php echo $testimoni_slide['tslider_designation'];?></span>
                                  </div>
                               </div>
                            </div>
                         </div>
                      </div>
-                     <div class="item">
-                        <div class="testimony-wrap py-4">
-                           <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
-                           <div class="text">
-                              <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                              <div class="d-flex align-items-center">
-                                 <div class="user-img" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/person_2.jpg)"></div>
-                                 <div class="pl-3">
-                                    <p class="name">Roger Scott</p>
-                                    <span class="position">Marketing Manager</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="item">
-                        <div class="testimony-wrap py-4">
-                           <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
-                           <div class="text">
-                              <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                              <div class="d-flex align-items-center">
-                                 <div class="user-img" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/person_3.jpg)"></div>
-                                 <div class="pl-3">
-                                    <p class="name">Roger Scott</p>
-                                    <span class="position">Marketing Manager</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="item">
-                        <div class="testimony-wrap py-4">
-                           <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
-                           <div class="text">
-                              <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                              <div class="d-flex align-items-center">
-                                 <div class="user-img" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/person_1.jpg)"></div>
-                                 <div class="pl-3">
-                                    <p class="name">Roger Scott</p>
-                                    <span class="position">Marketing Manager</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="item">
-                        <div class="testimony-wrap py-4">
-                           <div class="icon d-flex align-items-center justify-content-center"><span class="fa fa-quote-left"></span></div>
-                           <div class="text">
-                              <p class="mb-4">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-                              <div class="d-flex align-items-center">
-                                 <div class="user-img" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/images/person_2.jpg)"></div>
-                                 <div class="pl-3">
-                                    <p class="name">Roger Scott</p>
-                                    <span class="position">Marketing Manager</span>
-                                 </div>
-                              </div>
-                           </div>
-                        </div>
-                     </div>
+                     <?php
+
+                    }
+                    
+                    ?>
                   </div>
                </div>
             </div>
